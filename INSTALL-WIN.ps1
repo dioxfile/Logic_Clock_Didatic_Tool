a154f3fa74daba9ee3df186f7e43b5882aead312
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2026-04-14 20:41:10 -0400

    Update INSTALL-WIN.ps1

--- INSTALL-WIN.ps1
+++ INSTALL-WIN.ps1
@@ -65,7 +65,7 @@ function Verificar-Pip {
     }
 }
 
-$bibliotecas = @("wxPython", "netifaces", "python-dateutil", "pyPubSub", "ipaddr")
+$bibliotecas = @("wxPython", "netifaces", "python-dateutil", "pyPubSub", "ipaddress")
 
 function Verificar-Bibliotecas($bibliotecas) {
     foreach ($biblioteca in $bibliotecas) {
